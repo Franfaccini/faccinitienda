a30bd59b135f679f4746b2b17ccbfe717db42b9f
Franfaccini <88442287+Franfaccini@users.noreply.github.com>
2022-06-02 23:44:01 -0300

    agrega cosas al header

---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+const Main = () => {
+    return (
+        <p>Aca contenido de pagina</p>
+
+        
+    )
+}
+export default Main
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+    return (
+        <header>
+            <h1>SMP</h1>
+            <nav>
+                <a href="">Inicio</a>
+                <a href="">Contacto</a>
+                <a href="">Nosotros</a>
+            </nav>
+        </header>
+
+        
+    )
+}
+export default Navbar
