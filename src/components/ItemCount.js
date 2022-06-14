88582c6a7d1d98a436ba92693cf4011d05c2a278
Franfaccini <88442287+Franfaccini@users.noreply.github.com>
2022-06-14 11:00:43 -0300

    Agregando Hooks

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,32 +1,34 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 const ItemCount = ({ stock, initial, onAdd }) => {
 
-
   const [contador, setContador] = useState(initial)
 
+  useEffect(()=>{
+    
+    //console.log("Pidiendo usuarios...")
 
-
+  },[])
 
   const aumentarContador = () => {
-   
     setContador(contador + 1)
   }
 
   const bajarContador = () => {
-    setContador(contador - 1)
-  
+    setContador(0)
   }
 
-  const confirmarContador = () => { }
+  const confirmarContador = () => {
+    //onAdd(contador) ?
+   }
 
   return (
     <div>
       <p>El contador va : {contador}</p>
       <button onClick={aumentarContador}>aumentar</button>
       <button onClick={bajarContador}>disminuir</button>
-      <button>confirmar</button>
+      <button onClick={confirmarContador}>confirmar</button>
     </div>
   )
 }
